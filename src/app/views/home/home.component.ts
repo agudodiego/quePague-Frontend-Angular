9a6f03222ab805294bcd7bcc16792417c68322a2
piteca <piteca@pop-os.localdomain>
2026-09-20 15:15:15 -0300

    se cambia leyenda a MENSUAL

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -33,7 +33,7 @@ export class HomeComponent  {
   public formNote?: string;
 
   public meses: { id: number; nombre: string }[] = [
-    { id: 0, nombre: 'No Aplica' },
+    { id: 0, nombre: 'MENSUAL' },
     { id: 1, nombre: 'ENERO' },
     { id: 2, nombre: 'FEBRERO' },
     { id: 3, nombre: 'MARZO' },
